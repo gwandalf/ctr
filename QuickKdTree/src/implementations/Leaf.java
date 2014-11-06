0fa2eda22015b4f3cd615033cec5147df0335a71
gwandalf <gwlemoul@insa-rennes.fr>
2014-11-06 23:38:55 +0100

    Leaf

---
+++ QuickKdTree/src/implementations/Leaf.java
@@ -0,0 +1,23 @@
+package implementations;
+
+import interfaces.LeafInterface;
+import interfaces.RayInterface;
+
+public class Leaf extends Node implements LeafInterface {
+
+	/**
+	 * Constructor
+	 * @param origin
+	 * @param end
+	 */
+	public Leaf(Point origin, Point end) {
+		super(origin, end);
+	}
+
+	@Override
+	public RayInterface localRay(RayInterface totalRay) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
