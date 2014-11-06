0e90264020e0a41d1861e37d5ca625d44af24d53
gwandalf <gwlemoul@insa-rennes.fr>
2014-11-06 11:47:40 +0100

    Node

--- QuickKdTree/src/interfaces/NodeInterface.java
+++ QuickKdTree/src/interfaces/NodeInterface.java
@@ -1,5 +1,25 @@
 package interfaces;
 
+/**
+ * Node of a kd-tree, which subdivide a scene.
+ * Equivalent to a voxel.
+ * @author gwlemoul
+ *
+ */
 public interface NodeInterface extends GeometricElement {
 
+	/**
+	 * Computes the estimated cost defined by the SAH heuristic.
+	 * @param plan Candidate plan which may split the current node.
+	 * @return The estimated cost defined by the SAH heuristic.
+	 */
+	double sah(PlanInterface plan);
+	
+	/**
+	 * Computes the next voxel to be crossed by the given ray.
+	 * @param ray Ray which may cross a voxel.
+	 * @return The next voxel to be crossed by the given ray.
+	 */
+	LeafInterface nextVoxel(RayInterface ray);
+	
 }
