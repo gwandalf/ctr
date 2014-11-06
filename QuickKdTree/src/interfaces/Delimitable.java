0fb36e7c8b39c51fb8be03a2a72a838a8854cd5f
gwandalf <gwlemoul@insa-rennes.fr>
2014-11-06 11:31:26 +0100

    Delimitable

--- QuickKdTree/src/interfaces/Delimitable.java
+++ QuickKdTree/src/interfaces/Delimitable.java
@@ -1,10 +1,34 @@
 package interfaces;
 
+/**
+ * Represents an object which can be defined by two points.
+ * @author gwlemoul
+ *
+ */
 public interface Delimitable {
 
+	/**
+	 * Getter
+	 * @return origin
+	 */
 	PointInterface getOrigin();
+	
+	/**
+	 * Setter
+	 * @param origin
+	 */
 	void setOrigin(PointInterface origin);
+	
+	/**
+	 * Getter
+	 * @return end
+	 */
 	PointInterface getEnd();
+	
+	/**
+	 * Setter
+	 * @param end
+	 */
 	void setEnd(PointInterface end);
 	
 }
