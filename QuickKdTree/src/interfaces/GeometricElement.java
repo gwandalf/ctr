4926e0a0a9f2535af571f0e27b0de19ae755bcdd
gwandalf <gwlemoul@insa-rennes.fr>
2014-11-06 11:35:54 +0100

    GeometricElement

--- QuickKdTree/src/interfaces/GeometricElement.java
+++ QuickKdTree/src/interfaces/GeometricElement.java
@@ -1,9 +1,29 @@
 package interfaces;
 
+/**
+ * Element that can be crossed by a ray with a certain cost.
+ * @author gwlemoul
+ *
+ */
 public interface GeometricElement extends Delimitable {
 	
-	int getInnerCost();
-	void setInnerCost(int innerCost);
+	/**
+	 * Tells if the current object is the next to be crossed by the given ray.
+	 * @param ray Ray which maybe crosses the current object.
+	 * @return true if the current object is the next to be crossed by the given ray, false otherwise.
+	 */
 	boolean isNextCrossed(RayInterface ray);
+	
+	/**
+	 * Getter
+	 * @return cost
+	 */
+	int getInnerCost();
+	
+	/**
+	 * Setter
+	 * @param innerCost
+	 */
+	void setInnerCost(int innerCost);
 
 }
